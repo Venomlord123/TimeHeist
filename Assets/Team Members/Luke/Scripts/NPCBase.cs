abf22de3d4c5d4117e3d8eb155d5a10e38b1b6f7
Venomlord123 <67080748+Venomlord123@users.noreply.github.com>
2021-07-14 14:22:26 +1000

    Updates to NPCBase

--- Assets/Team Members/Luke/Scripts/NPCBase.cs
+++ Assets/Team Members/Luke/Scripts/NPCBase.cs
@@ -13,5 +13,13 @@ namespace Luke
         public List<Waypoint> waypointPath;
         [Tooltip("Wait times for each waypoint")]
         public List<float> waypointWaitTimes;
+        
+        //Details for NPC's
+        [Tooltip("Name of the NPC")]
+        public string npcName;
+        [Tooltip("Where the NPC's have been")]
+        public string locationsBeen;
+        [Tooltip("What has been said in the conversations that the NPC has interacted with")]
+        public string conversations;
     }
 }
