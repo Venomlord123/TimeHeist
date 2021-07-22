0821f367125a28dc8e21d06474a940cb34ce953f
Venomlord123 <1019559@student.sae.edu.au>
2021-07-22 17:35:59 +1000

    Updated UI Code

--- Assets/Team Members/Zach/Scripts/UISuspectPanel.cs
+++ Assets/Team Members/Zach/Scripts/UISuspectPanel.cs
@@ -11,22 +11,39 @@ namespace ZachFrench
     {
 
         public List<NPCBase> npcBases;
-        public NPCBase currentNPC;
+        public int currentNpc;
+        public NPCBase currentNPCSelected;
+        public bool nextSuspect;
         public TextMeshProUGUI suspectName;
         public TextMeshProUGUI suspectLocations;
         
         // Start is called before the first frame update
         void Start()
         {
-           npcBases.Add(FindObjectOfType<NPCBase>());
-           currentNPC = npcBases[0];
+            currentNpc = 0;
+            currentNPCSelected = npcBases[currentNpc];
+            updateSuspects();
         }
 
         // Update is called once per frame
         void Update()
         {
-            suspectName.text = currentNPC.npcName;
-            suspectLocations.text = currentNPC.locationsBeen;
+            if (nextSuspect)
+            {
+                currentNpc++;
+                if (currentNpc < npcBases.Count)
+                {
+                    currentNPCSelected = npcBases[currentNpc];
+                    updateSuspects();
+                }
+                nextSuspect = false;
+            }
+        }
+
+        public void updateSuspects()
+        {
+            suspectName.text = currentNPCSelected.npcName;
+            suspectLocations.text = currentNPCSelected.locationsBeen;
         }
     }
 }
