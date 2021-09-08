93e4fe41e2e0c8215685627ed1042c74b9314c0a
Venomlord123 <1019559@student.sae.edu.au>
2021-09-08 16:13:15 +1000

    Suspect Page with spawning prefabs

---
+++ Assets/Team Members/Zach/Scripts/SuspectPage.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SuspectPage : MonoBehaviour
+{
+    public PlayerJournal playerJournal;
+    public List<NPCInfomation> npcInfos;
+    public NPCInfomation npcInformation;
+    public GameObject suspectReference;
+    public List<GameObject> suspectEntries;
+    
+    //Hack
+    public bool testing = false;
+
+
+    public void Start()
+    {
+        npcInfos = playerJournal.npcInformation;
+    }
+
+
+    private void Update()
+    {
+        if (testing == true)
+        {
+            CreateSuspectReferences();
+        }
+    }
+
+    public void CreateSuspectReferences()
+    {
+        foreach (NPCInfomation npcInfo in npcInfos)
+        {
+            GameObject tempSuspectEntry = Instantiate(suspectReference,gameObject.transform);
+            suspectEntries.Add(tempSuspectEntry);
+            tempSuspectEntry.GetComponentInChildren<RawImage>().texture = npcInfo.mugShot;
+            tempSuspectEntry.GetComponentInChildren<TextMeshProUGUI>().text = npcInfo.suspectName;
+            testing = false;
+        }
+    }
+}
