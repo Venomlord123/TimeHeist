d1f71e9cc7cf1b753413c3320721eccb84967ea4
lukeyboi-byte <67080741+lukeyboi-byte@users.noreply.github.com>
2021-09-08 15:00:22 +1000

    waypoint face direction line for npc

--- Assets/Team Members/Luke/Scripts/PatrolManager.cs
+++ Assets/Team Members/Luke/Scripts/PatrolManager.cs
@@ -9,17 +9,22 @@ namespace Luke
         //variables
         public List<Waypoint> NPCWayPoints;
         public List<Waypoint> NPCExitWaypoints;
+        public float rayLength = 5f;
         
         // Start is called before the first frame update
         void Start()
         {
-            NPCWayPoints.AddRange(GetComponents<Waypoint>());
+            NPCWayPoints.AddRange(FindObjectsOfType<Waypoint>());
         }
 
         // Update is called once per frame
         void Update()
         {
-
+            foreach (Waypoint npcWayPoint in NPCWayPoints)
+            {
+                Vector3 forward = transform.TransformDirection(Vector3.forward) * rayLength;
+                Debug.DrawRay(npcWayPoint.transform.position,forward,Color.blue);
+            }
         }
     }
 }
