c366baccae82f54f4c025391ffa7ac7cd1452764
Venomlord123 <67080748+Venomlord123@users.noreply.github.com>
2021-11-05 11:12:52 +1100

    Pause Code Exists in its basest form (Contact me if need help)

---
+++ Assets/Team Members/Zach/Scripts/Pause.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        pauseMenu.SetActive(true);
+    }
+
+    public void UnpauseGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
