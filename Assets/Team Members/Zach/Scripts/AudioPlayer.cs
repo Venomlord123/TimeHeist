80e08586593e6eacd2212851632b0ad60d488f3d
Venomlord123 <67080748+Venomlord123@users.noreply.github.com>
2021-11-05 14:37:26 +1100

    Audio for fire alarm in scene

---
+++ Assets/Team Members/Zach/Scripts/AudioPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+   public GameManager gameManager;
+   public AudioSource audioSource;
+
+   private void OnEnable()
+   {
+      gameManager.timer.FireAlarmEvent += FireAlarm;
+   }
+
+   private void OnDisable()
+   {
+      gameManager.timer.FireAlarmEvent -= FireAlarm;
+   }
+
+   private void FireAlarm()
+   {
+      audioSource.Play();
+   }
+}
